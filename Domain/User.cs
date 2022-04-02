b4605b5b73f16e4d4367e0e1189ecce01e3b5e8a
HumanMu <abdullah_shirdaghi_1994@yahoo.dk>
2022-04-02 22:27:39 +0200

    Database and the structure of the project is setup

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
     public class User
     {
+        [Key]
+        public int Id {get; set;} 
         public string FirstName {get; set;} 
         public string LastName {get; set;} 
         public int Age {get; set;}
         public string Gender {get; set;}
-        public Boolean status {get; set;} // status om online/offline
+
     }
 }
---
+++ Persistence/Migrations/20220402201446_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Gender = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Age", "FirstName", "Gender", "LastName" },
+                values: new object[] { 1, 29, "Human", "Man", "Muzaffari" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
